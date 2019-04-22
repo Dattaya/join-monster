f7dc84f988373ab11e0e566105453b69629ab419
Yaroslav Kiliba <om.dattaya@gmail.com>
2019-04-22 12:33:34 +0300

    chore: rebuild

---
+++ dist/graphql.d.ts
@@ -0,0 +1,5 @@
+import * as graphql from 'graphql'
+
+export class GraphQLObjectType extends graphql.GraphQLObjectType {}
+export class GraphQLUnionType extends graphql.GraphQLUnionType {}
+export class GraphQLInterfaceType extends graphql.GraphQLInterfaceType {}
--- dist/graphql/index.js
+++ dist/graphql/index.js
@@ -5,15 +5,17 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.GraphQLInterfaceType = exports.GraphQLUnionType = exports.GraphQLObjectType = void 0;
 
-var _graphql = require("graphql");
+var graphql = _interopRequireWildcard(require("graphql"));
 
 var _pick = _interopRequireDefault(require("lodash/pick"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
 const keys = ['alwaysFetch', 'sqlTable', 'uniqueKey'];
 
-class GraphQLObjectType extends _graphql.GraphQLObjectType {
+class GraphQLObjectType extends graphql.GraphQLObjectType {
   constructor(objectTypeConfig) {
     const joinMonsterConfig = (0, _pick.default)(objectTypeConfig, keys);
     super(objectTypeConfig);
@@ -24,7 +26,7 @@ class GraphQLObjectType extends _graphql.GraphQLObjectType {
 
 exports.GraphQLObjectType = GraphQLObjectType;
 
-class GraphQLUnionType extends _graphql.GraphQLUnionType {
+class GraphQLUnionType extends graphql.GraphQLUnionType {
   constructor(objectTypeConfig) {
     const joinMonsterConfig = (0, _pick.default)(objectTypeConfig, keys);
     super(objectTypeConfig);
@@ -35,7 +37,7 @@ class GraphQLUnionType extends _graphql.GraphQLUnionType {
 
 exports.GraphQLUnionType = GraphQLUnionType;
 
-class GraphQLInterfaceType extends _graphql.GraphQLInterfaceType {
+class GraphQLInterfaceType extends graphql.GraphQLInterfaceType {
   constructor(objectTypeConfig) {
     const joinMonsterConfig = (0, _pick.default)(objectTypeConfig, keys);
     super(objectTypeConfig);
